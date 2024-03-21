859119f38b4a53251c99c33b6d05b50787b82e6c
Marcin Dudek <manekd@gmail.com>
2024-03-21 10:28:21 +0100

    Page built

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2 class="text-4xl font-bold">Under development</h2>
+
+<div class="p-4">
+	<img
+		class="rounded-box"
+		src="/under-development.webp"
+		alt="Three birds which hare building website."
+	/>
+</div>
